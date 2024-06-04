cf11aad055c48aff7e23732c4527f37c6c250edc
김동균 <wooheuy5752@nate.com>
2024-06-04 21:38:32 +0900

    FEAT : 로그인 버튼 추가

--- donggyun/src/components/Input/index.jsx
+++ donggyun/src/components/Input/index.jsx
@@ -6,25 +6,33 @@ import { useState } from 'react';
 const Input = () => {
   const [password, setPassword] = useState('');
   const handleChange = (e) => setPassword(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(password);
+  };
 
   return (
-    <form className="input-form">
-      <label className="input-label" htmlFor="password">
-        Password
-      </label>
-      <div className="input-box">
-        <MdOutlineLock size={18} color="grey" />
-        <input
-          className="input-content"
-          type="password"
-          id="password"
-          value={password}
-          placeholder="비밀번호를 입력해주세요."
-          onChange={handleChange}
-        />
-        <FiEyeOff size={18} color="grey" />
+    <form className="input-form" onSubmit={handleSubmit}>
+      <div className="input-password">
+        <label className="input-password__label" htmlFor="password">
+          Password
+        </label>
+        <div className="input-password__box">
+          <MdOutlineLock size={18} color="grey" />
+          <input
+            className="input-password__content"
+            type="password"
+            id="password"
+            value={password}
+            placeholder="비밀번호를 입력해주세요."
+            onChange={handleChange}
+          />
+          <FiEyeOff size={18} color="grey" />
+        </div>
+        <p className="input-password__helpler">Helper text</p>
       </div>
-      <p className="input-helpler">Helper text</p>
+
+      <button className="input-button">로그인</button>
     </form>
   );
 };
