026e5b0286b3be97056af0c7df00b16159b46ec6
김동균 <wooheuy5752@nate.com>
2024-06-04 21:20:55 +0900

    FEAT : UI 구현 완료

--- donggyun/src/App.jsx
+++ donggyun/src/App.jsx
@@ -1,10 +1,11 @@
 import Input from './components/Input';
+import './App.css';
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Input />
-    </>
+    </div>
   );
 }
 
--- donggyun/src/components/Input/index.jsx
+++ donggyun/src/components/Input/index.jsx
@@ -1,8 +1,25 @@
+import { MdOutlineLock } from 'react-icons/md';
+import { FiEyeOff } from 'react-icons/fi';
+import './style.css';
+
 const Input = () => {
   return (
-    <div>
-      <div></div>
-    </div>
+    <form className="input-form">
+      <label className="input-label" htmlFor="password">
+        Password
+      </label>
+      <div className="input-box">
+        <MdOutlineLock size={18} color="grey" />
+        <input
+          className="input-content"
+          type="password"
+          id="password"
+          placeholder="비밀번호를 입력해주세요."
+        />
+        <FiEyeOff size={18} color="grey" />
+      </div>
+      <p className="input-helpler">Helper text</p>
+    </form>
   );
 };
 
