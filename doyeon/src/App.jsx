a46fc0e54264b91ef980841e687c382667d50464
dykimruby <130230167+dykimruby@users.noreply.github.com>
2024-06-18 20:40:33 +0900

    test

---
+++ doyeon/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Input from './components/input';
+
+function App() {
+  return (
+    <div className="container">
+      <Input/>
+    </div>
+  );
+}
+
+export default App;
