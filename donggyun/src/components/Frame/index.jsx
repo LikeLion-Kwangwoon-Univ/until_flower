ff66e3e4bb9ba8e3854fd00c7a3d0608767eb2d0
김동균 <wooheuy5752@nate.com>
2024-06-23 15:59:53 +0900

    TURNNING

--- donggyun/src/components/Frame/index.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Frame = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  padding: 13px 10px;
-  background-color: white;
-`;
